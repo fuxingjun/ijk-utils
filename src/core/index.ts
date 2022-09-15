35d005ed645783fee203013f74be5d6851c5ffa9
fuxingjun <jayden.fu@longsys.com>
2022-09-15 10:30:39 +0800

    list2Tree

--- src/core/index.ts
+++ src/core/index.ts
@@ -123,3 +123,76 @@ export const getTextWidth = (function () {
     return metrics.width;
   };
 })();
+
+interface TreeOption {
+  parentIdKey?: string;
+  idKey?: string;
+  nameKey?: string;
+  /**
+   * 判断是否根节点的方法
+   */
+  isRootNode?: Function;
+}
+
+/**
+ * 默认选项
+ */
+const defaultOption = {
+  parentIdKey: 'parentId',
+  idKey: 'id',
+  nameKey: 'name',
+  isRootNode(parentId: number | string): boolean {
+    return parentId === 0;
+  }
+};
+
+/**
+ * 数组转树结构
+ * @param {*} list 原数组
+ * @param {*} option 配置项 parentIdKey: parentId的key, idKey: id的key, nameKey: name的key, isRootNode: 判断是否为根节点的方法
+ * @returns {Record<string, any>[]}
+ */
+export function list2Tree(
+  list: Record<string, any>[],
+  option: TreeOption = defaultOption
+): Record<string, any>[] {
+  const parentIdKey = option.parentIdKey || defaultOption.parentIdKey;
+  const idKey = option.idKey || defaultOption.idKey;
+  const nameKey = option.nameKey || defaultOption.nameKey;
+  const isRootNode = option.isRootNode || defaultOption.isRootNode;
+  const map = new Map();
+  const result: Record<string, any>[] = [];
+  list.forEach(item => {
+    // 如果是根节点, push 到结果
+    if (isRootNode(item[parentIdKey])) {
+      result.push(item);
+    } else {
+      // 如果父节点已存到map, 将节点存到父节点children
+      if (map.has(item[parentIdKey])) {
+        const parent = map.get(item[parentIdKey]);
+        if (!parent.children) {
+          parent.children = [];
+        }
+        parent.children.push(item);
+      } else {
+        // 如果父节点还没有存到map, 用id 和children组成对象存入map, 后面遍历到父节点时, 再存入其它信息
+        map.set(item[parentIdKey], {
+          id: item[parentIdKey],
+          children: [item],
+        });
+      }
+    }
+    // 如果当前节点没有存到map, 存到map
+    if (!map.has(item[idKey])) {
+      map.set(item[idKey], item);
+    } else {
+      const current = map.get(item[idKey]);
+      // 如果map中已存在item, 判断是否是前面遍历时只存了id和children, 是的话将children合并进去
+      if (!current[nameKey]) {
+        item.children = current.children;
+        map.set(item[idKey], item);
+      }
+    }
+  });
+  return result;
+}
