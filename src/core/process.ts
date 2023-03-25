7568e0216d709485cab7dfa55d1070fd7a173700
fuxingjun <fuxingjun@qq.com>
2023-03-25 12:47:57 +0800

    fix processArgs

--- src/core/process.ts
+++ src/core/process.ts
@@ -5,7 +5,9 @@ export function getProcessArgs(): Record<string, string> {
   const args = process.argv.slice(2);
   const result: Record<string, string> = {};
   args.forEach(item => {
-    const [key, value] = item.split("=")
+    const splitIndex = item.indexOf("=")
+    const key = item.slice(0, splitIndex)
+    const value = item.slice(splitIndex + 1)
     if (key.indexOf("--") === 0) {
       result[key.slice(2)] = value;
     }
